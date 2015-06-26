abdf18c600e9e72f5d196196ab940158683c22dc
Luca Tumedei <luca@theaveragedev.com>
2015-06-26 08:36:33 +0200

    removed unused User method

--- src/trc/User.php
+++ src/trc/User.php
@@ -27,16 +27,6 @@ class trc_User {
 		return apply_filters( 'trc_user_content_access_slugs', $slugs, get_current_user() );
 	}
 
-	public function can_access_template( $template ) {
-		$can_access = false;
-
-		if ( $this->wp_user->has_cap( 'edit_other_posts' ) ) {
-			$can_access = true;
-		}
-
-		return apply_filters( 'trc_user_can_access_template', $can_access, $template, $this->wp_user );
-	}
-
 	public function can_access_query( WP_Query $query ) {
 		$can_access = false;
 
