a27737e2d5efa06b46b54c7890537b2dd1151a03
Luca Tumedei <luca@theaveragedev.com>
2015-06-30 16:40:37 +0200

    removed unused plugin var

--- src/trc/Core/Plugin.php
+++ src/trc/Core/Plugin.php
@@ -19,11 +19,6 @@ class trc_Core_Plugin {
 	public $url;
 
 	/**
-	 * @var string The restriction taxonomy name.
-	 */
-	public $restriction_taxonomy = 'trc_content_restriction';
-
-	/**
 	 * @var string The user meta key storing the content access slugs.
 	 */
 	public $user_content_access_slug_meta_key = '_trc_content_access_slug';
