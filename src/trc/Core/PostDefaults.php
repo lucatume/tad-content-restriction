29441e973643499c876a8f38bcc2904beefb1b6b
Luca Tumedei <luca@theaveragedev.com>
2015-08-04 18:11:47 +0200

    small post defaults refactor

--- src/trc/Core/PostDefaults.php
+++ src/trc/Core/PostDefaults.php
@@ -32,7 +32,10 @@ class trc_Core_PostDefaults {
 		 * @param WP_Post $post    Post object.
 		 * @param bool    $update  Whether this is an existing post being updated or not.
 		 */
-		add_action( 'wp_insert_post', array( $this, 'apply_default_terms' ), 99, 3 );
+		add_action( 'wp_insert_post', array(
+			$this,
+			'apply_default_terms'
+		), 99, 3 );
 	}
 
 	public function set_user_slug_provider_for( $taxonomy, trc_Public_UserSlugProviderInterface $user_slug_provider ) {
@@ -44,6 +47,11 @@ class trc_Core_PostDefaults {
 			// if editing an existing posts defaults are either in place or the user manually set them
 			return;
 		}
+		$this->apply_default_restrictions( $post_id, $post );
+	}
+
+	public function apply_default_restrictions( $post_id, WP_Post $post = null ) {
+		$post = empty( $post ) ? get_post( $post_id ) : $post;
 		foreach ( $this->user_slug_providers as $taxonomy => $slug_provider ) {
 			wp_set_object_terms( $post_id, $slug_provider->get_default_post_terms( $post ), $taxonomy, true );
 		}
