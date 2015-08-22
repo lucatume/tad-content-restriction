9ac025ae9e4448d84f730af2a537efcbad75d26e
Luca Tumedei <luca@theaveragedev.com>
2015-08-22 10:35:08 +0200

    updated dependencies

--- vendor/lucatume/tad-reschedule/tests/_bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This is global bootstrap for autoloading
-use tad\FunctionMocker\FunctionMocker;
-
-include_once dirname( dirname( __FILE__ ) ) . '/tad-reschedule.php';
-FunctionMocker::init();
